c4cc3c91e4221ea2997a3b5c66ac1d7ec17217a7
masoud <masoud.red64@gamil.com>
2022-12-10 10:28:55 -0800

    header template 243

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 export default function App() {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+      </div>
+    </>
+  );
 }
