079c17dcb42dd5be1f6e2b27713ea47668372152
masoud <masoud.red64@gamil.com>
2022-12-10 08:33:40 -0800

    sidebar template 240

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <Sidebar />;
 }
