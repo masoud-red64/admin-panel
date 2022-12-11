5190e040917293eac9579508df3fffb6e6e52172
masoud <masoud.red64@gamil.com>
2022-12-11 09:13:33 -0800

    add new product template 248

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ErrorBox from "../ErrorBox/ErrorBox";
-
+import AddNewProduct from "./AddNewProduct/AddNewProduct";
 export default function Products() {
-  return <ErrorBox msg={"هیج محصولی یافت نشد"} />;
+  return (
+    <>
+      <ErrorBox msg={"هیج محصولی یافت نشد"} />
+
+      <AddNewProduct />
+    </>
+  );
 }
