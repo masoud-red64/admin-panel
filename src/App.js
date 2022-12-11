125de40873177b53460fdc9d03e747ff23168d38
masoud <masoud.red64@gamil.com>
2022-12-11 03:17:40 -0800

    refactor router with useRoutes 246

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import "./App.css";
 import Header from "./Components/Header/Header";
-import { Route, Routes } from "react-router-dom";
-import Products from "./Components/Products/Products";
-import Comments from "./Components/Comments/Comments";
-import Users from "./Components/Users/Users";
-import Orders from "./Components/Orders/Orders";
-import Offs from "./Components/Offs/Offs";
+import routes from "./routes";
+import { useRoutes } from "react-router-dom";
 
 export default function App() {
+  const router = useRoutes(routes);
   return (
     <>
       <Sidebar />
@@ -17,13 +14,7 @@ export default function App() {
       <div className="main">
         <Header />
 
-        <Routes>
-          <Route path="/products" element={<Products />} />
-          <Route path="/comments" element={<Comments />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/offs" element={<Offs />} />
-        </Routes>
+        {router}
       </div>
     </>
   );
