e93615e453bee4d1dd2fcbc8a09199558f76124b
masoud <masoud.red64@gamil.com>
2022-12-12 04:35:26 -0800

    delete modal component with portal 253

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -1,6 +1,12 @@
 import React from "react";
+import DeleteModal from "../DeleteModal/DeleteModal";
 import ErrorBox from "../ErrorBox/ErrorBox";
 
 export default function Comments() {
-  return <ErrorBox msg={"هیج کامنتی یافت نشد"} />;
+  return (
+    <>
+      <ErrorBox msg={"هیج کامنتی یافت نشد"} />
+      <DeleteModal />
+    </>
+  );
 }
