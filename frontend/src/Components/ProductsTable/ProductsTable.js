6c6af08f9a651ebac105b21d0078de2e40d20a4e
masoud <masoud.red64@gamil.com>
2022-12-15 18:24:21 +0330

    get products from api

--- frontend/src/Components/ProductsTable/ProductsTable.js
+++ frontend/src/Components/ProductsTable/ProductsTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsTable.css";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import DetailsModal from "../DetailsModal/DetailsModal";
@@ -9,6 +9,16 @@ export default function ProductsTable() {
   const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
   const [isShowDetailsModal, setIsShowDetailsModal] = useState(false);
   const [isShowEditModal, setIsShowEditModal] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/api/products/")
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((products) => setAllProducts(products));
+  }, []);
 
   const deleteModalSubmitAction = () => {
     setIsShowDeleteModal(false);
@@ -37,39 +47,42 @@ export default function ProductsTable() {
             <th>موجودی</th>
           </tr>
         </thead>
+
         <tbody>
-          <tr className="products-table-tr">
-            <td>
-              <img
-                src="/images/oil.jpeg"
-                alt="oil image"
-                className="products-table-img"
-              />
-            </td>
-            <td>روغن سرخ کردنی</td>
-            <td>92000 تومان</td>
-            <td>82</td>
-            <td>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowDetailsModal(true)}
-              >
-                جزییات
-              </button>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowDeleteModal(true)}
-              >
-                حذف
-              </button>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowEditModal(true)}
-              >
-                ویرایش
-              </button>
-            </td>
-          </tr>
+          {allProducts.map((product) => (
+            <tr key={product.id} className="products-table-tr">
+              <td>
+                <img
+                  src={product.img}
+                  alt="oil image"
+                  className="products-table-img"
+                />
+              </td>
+              <td>{product.title}</td>
+              <td>{product.price} تومان</td>
+              <td>{product.count}</td>
+              <td>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowDetailsModal(true)}
+                >
+                  جزییات
+                </button>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowDeleteModal(true)}
+                >
+                  حذف
+                </button>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowEditModal(true)}
+                >
+                  ویرایش
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       {isShowDeleteModal && (
