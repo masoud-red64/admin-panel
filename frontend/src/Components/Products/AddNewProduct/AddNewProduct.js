4b5a5421e4ae36540b862d4150c64d1b4a2f0420
masoud <masoud.red64@gamil.com>
2022-12-16 16:02:17 +0330

    add new product feature 272

--- frontend/src/Components/Products/AddNewProduct/AddNewProduct.js
+++ frontend/src/Components/Products/AddNewProduct/AddNewProduct.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddNewProduct.css";
 import { BsCursorText } from "react-icons/bs";
 import { BiDollar } from "react-icons/bi";
@@ -7,8 +7,65 @@ import { AiOutlineFolderAdd } from "react-icons/ai";
 import { GiSelfLove } from "react-icons/gi";
 import { FaSellsy } from "react-icons/fa";
 import { IoMdColorPalette } from "react-icons/io";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function AddNewProduct() {
+  const [newProductTitle, setNewProductTitle] = useState("");
+  const [newProductPrice, setNewProductPrice] = useState("");
+  const [newProductCount, setNewProductCount] = useState("");
+  const [newProductImg, setNewProductImg] = useState("");
+  const [newProductPopularity, setNewProductPopularity] = useState("");
+  const [newProductSale, setNewProductSale] = useState("");
+  const [newProductColors, setNewProductColors] = useState("");
+
+  const addNotify = () =>
+    toast.success("😎محصول با موفقیت اضافه شد", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  const newProduct = {
+    title: newProductTitle,
+    price: newProductPrice,
+    count: newProductCount,
+    img: newProductImg,
+    popularity: newProductPopularity,
+    sale: newProductSale,
+    colors: newProductColors,
+  };
+  const addNewProduct = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:7000/api/products/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+    if (
+      newProductTitle &&
+      newProductPrice &&
+      newProductCount &&
+      newProductImg &&
+      newProductPopularity &&
+      newProductSale &&
+      newProductColors
+    ) {
+      addNotify();
+    } else {
+      alert("همه ی موارد را تکمیل کنید");
+    }
+  };
+
   return (
     <div className="products-main">
       <h1 className="products-title">افزودن محصول جدید</h1>
@@ -21,6 +78,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="اسم محصول را بنویسید"
               className="add-product-input"
+              value={newProductTitle}
+              onChange={(event) => setNewProductTitle(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -29,6 +88,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="قیمت محصول را بنویسید"
               className="add-product-input"
+              value={newProductPrice}
+              onChange={(event) => setNewProductPrice(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -37,6 +98,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="موجودی محصول را بنویسید"
               className="add-product-input"
+              value={newProductCount}
+              onChange={(event) => setNewProductCount(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -45,6 +108,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="آدرس عکس محصول را بنویسید"
               className="add-product-input"
+              value={newProductImg}
+              onChange={(event) => setNewProductImg(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -53,6 +118,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="میزان محبوبیت محصول را بنویسید"
               className="add-product-input"
+              value={newProductPopularity}
+              onChange={(event) => setNewProductPopularity(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -61,6 +128,8 @@ export default function AddNewProduct() {
               type="text"
               placeholder="میزان فروش محصول را بنویسید"
               className="add-product-input"
+              value={newProductSale}
+              onChange={(event) => setNewProductSale(event.target.value)}
             />
           </div>
           <div className="add-products-form-group">
@@ -69,11 +138,18 @@ export default function AddNewProduct() {
               type="text"
               placeholder="تعداد رنگ بندی محصول را بنویسید"
               className="add-product-input"
+              value={newProductColors}
+              onChange={(event) => setNewProductColors(event.target.value)}
             />
           </div>
         </div>
-        <button className="add-products-btn">ثبت محصول</button>
+        <button className="add-products-btn" onClick={addNewProduct}>
+          ثبت محصول
+        </button>
       </form>
+      <div>
+        <ToastContainer />
+      </div>
     </div>
   );
 }
