858f6b6368cba2e11ce357bc4ab3d6978bc371cc
masoud <masoud.red64@gamil.com>
2022-12-16 21:38:46 +0330

    show users list 284

--- frontend/src/Components/Users/Users.js
+++ frontend/src/Components/Users/Users.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ErrorBox from "../ErrorBox/ErrorBox";
+import "./Users.css";
 
 export default function Users() {
-  return <ErrorBox msg={"هیج کاربری یافت نشد"} />;
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/api/users")
+      .then((res) => res.json())
+      .then((users) => setAllUsers(users));
+  }, []);
+
+  return (
+    <div className="cms-main">
+      <h1 className="cms-title">لیست کاربران</h1>
+      {allUsers.length ? (
+        <table className="cms-table">
+          <thead>
+            <tr>
+              <th>نام </th>
+              <th>نام خانوادگی</th>
+              <th>یوزنیم</th>
+              <th>رمز عبور</th>
+              <th>شماره تماس</th>
+              <th>ایمیل</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers.map((user) => (
+              <tr key={user.id}>
+                <td>{user.firsname}</td>
+                <td>{user.lastname}</td>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="products-table-btn">حذف</button>
+                  <button className="products-table-btn">جزییات</button>
+                  <button className="products-table-btn">ویرایش</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <ErrorBox msg={"هیج کاربری یافت نشد"} />
+      )}
+    </div>
+  );
 }
